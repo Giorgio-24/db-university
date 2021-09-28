adcdf180a3a5c442ef5377f339ddef177514c399
Giorgio Andrei <cipuandrei@libero.it>
2021-09-28 20:27:06 +0200

    courses-per-department-added

--- file.sql
+++ file.sql
@@ -55,3 +55,6 @@ FROM `exam_student`
 GROUP BY `exam_id`;
 
 --^12) Contare quanti corsi di laurea ci sono per ogni dipartimento.
+SELECT COUNT(*), `department_id`
+FROM `degrees`
+GROUP BY `department_id`;
