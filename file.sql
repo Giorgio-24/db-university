358fd8d6e81c024188fe0e37fd98e5ac9359006f
Giorgio Andrei <cipuandrei@libero.it>
2021-09-28 17:24:32 +0200

    number-of-departments-in-uni-discovered

--- file.sql
+++ file.sql
@@ -15,17 +15,20 @@ WHERE YEAR(`date_of_birth`) >= 1990;
 --Mi viene da dire che questa soluzione al momento è giusta cosi, ma il prossimo anno non lo sarà più perchè 
 --andranno inclusi anche i nati nel 1991 e cosi per ogni anno a venire.
 
---^4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286).
+--^4) Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286).
 SELECT `id`, `name`, `period`, `year`
 FROM `courses`
 WHERE `year` = 1 AND `period` = 'I semestre';
 
---^5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21).
+--^5) Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21).
 SELECT `id`, `course_id`, `date`, `hour`
 FROM `exams`
 WHERE `date`= '2020/06/20' AND hour(`hour`) >= 14;
 
---^6. Selezionare tutti i corsi di laurea magistrale (38)
+--^6) Selezionare tutti i corsi di laurea magistrale (38).
 SELECT `id`, `department_id`, `name`, `level`
 FROM `degrees`
 WHERE `level` = 'magistrale';
+
+--^7) Da quanti dipartimenti è composta l'università? (12).
+SELECT COUNT(*) FROM `departments`;
