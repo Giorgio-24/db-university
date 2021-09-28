582db2c6e61e26cbaf2b636a11683d9c416e5c63
Giorgio Andrei <cipuandrei@libero.it>
2021-09-28 17:16:52 +0200

    only-magistral-degrees-showed

--- file.sql
+++ file.sql
@@ -1,7 +1,7 @@
 --^ 1) Selezionare tutti gli studenti nati nel 1990 (160).
 SELECT `id`, `name`, `surname`, `date_of_birth`
 FROM `students`
-WHERE YEAR(`date_of_birth`) = '1990';
+WHERE YEAR(`date_of_birth`) = 1990;
 
 --^ 2) Selezionare tutti i corsi che valgono più di 10 crediti (479).
 SELECT `id`, `degree_id`, `name`, `cfu`
@@ -11,7 +11,7 @@ WHERE `cfu` >= 10;
 --^3) Selezionare tutti gli studenti che hanno più di 30 anni.
 SELECT `id`, `name`, `surname`, `date_of_birth`
 FROM `students`
-WHERE YEAR(`date_of_birth`) >= '1990';
+WHERE YEAR(`date_of_birth`) >= 1990;
 --Mi viene da dire che questa soluzione al momento è giusta cosi, ma il prossimo anno non lo sarà più perchè 
 --andranno inclusi anche i nati nel 1991 e cosi per ogni anno a venire.
 
@@ -24,3 +24,8 @@ WHERE `year` = 1 AND `period` = 'I semestre';
 SELECT `id`, `course_id`, `date`, `hour`
 FROM `exams`
 WHERE `date`= '2020/06/20' AND hour(`hour`) >= 14;
+
+--^6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT `id`, `department_id`, `name`, `level`
+FROM `degrees`
+WHERE `level` = 'magistrale';
