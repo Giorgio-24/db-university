92c0360bea414610f4714a80368f1a7c379f12d5
Giorgio Andrei <cipuandrei@libero.it>
2021-09-28 16:29:38 +0200

    +30-years-old-students-added

--- file.sql
+++ file.sql
@@ -8,3 +8,10 @@ SELECT `id`, `degree_id`, `name`, `cfu`
 FROM `courses`
 WHERE `cfu` >= 10;
 
+--^3) Selezionare tutti gli studenti che hanno più di 30 anni.
+
+SELECT `id`, `name`, `surname`, `date_of_birth`
+FROM `students`
+WHERE YEAR(`date_of_birth`) >= '1990';
+--Mi viene da dire che questa soluzione al momento è giusta cosi, ma il prossimo anno non lo sarà più perchè 
+--andranno inclusi anche i nati nel 1991 e cosi per ogni anno a venire.
