bc85c883fdc7507115ee8b19b7bf08fe33d46698
Giorgio Andrei <cipuandrei@libero.it>
2021-09-28 20:50:05 +0200

    over-30-verification-is-now-future-proof

--- file.sql
+++ file.sql
@@ -11,9 +11,8 @@ WHERE `cfu` >= 10;
 --^3) Selezionare tutti gli studenti che hanno più di 30 anni.
 SELECT `id`, `name`, `surname`, `date_of_birth`
 FROM `students`
-WHERE YEAR(`date_of_birth`) >= 1990;
---Mi viene da dire che questa soluzione al momento è giusta cosi, ma il prossimo anno non lo sarà più perchè 
---andranno inclusi anche i nati nel 1991 e cosi per ogni anno a venire.
+WHERE YEAR(CURRENT_DATE()) - (YEAR(`date_of_birth`)) > 30  
+ORDER BY `students`.`date_of_birth`  DESC;
 
 --^4) Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286).
 SELECT `id`, `name`, `period`, `year`
